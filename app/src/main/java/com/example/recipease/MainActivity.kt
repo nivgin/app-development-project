74681c25915606c34d86a67079e2fe0d97176859
nivgin <niv1@hiker.org.il>
2026-02-13 18:46:00 +0200

    added fragment, and a navbar

--- app/src/main/java/com/example/recipease/MainActivity.kt
+++ app/src/main/java/com/example/recipease/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.recipease
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_navhost_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val navbar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        NavigationUI.setupWithNavController(navbar, navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> navController.navigateUp()
+            else -> NavigationUI.onNavDestinationSelected(
+                item,
+                navController
+            )
         }
     }
 }
