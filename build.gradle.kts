da39c406d4c27212871841e25623bf6e9dc35bba
nivgin <niv1@hiker.org.il>
2026-02-13 18:54:36 +0200

    added layout

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all modules
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.android.library") version "7.3.1" apply false
+}
+
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+    }
 }
